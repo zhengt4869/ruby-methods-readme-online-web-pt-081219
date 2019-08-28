3413cc48fd6ed3afb24b610a1a53142b80a16bb5
KG ZZ <czheng5h88@gmail.com>
2019-08-28 16:53:47 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,5 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
+
 phrase = "Hello World!"
 puts phrase
 puts phrase
